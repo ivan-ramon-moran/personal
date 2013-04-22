4a6e43f8da8e75b27e39421e7ccf1f07c8479087
Ivan Ramon Moran <ivan.ramon.moran@gmail.com>
2013-04-22 16:39:28 +0200

    Subida

---
+++ Pila/src/Main.cpp
@@ -0,0 +1,20 @@
+//============================================================================
+// Name        : Pila.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include "Pila.cpp"
+using namespace std;
+
+int main()
+{
+	int numero = 10;
+
+	cout << hex << numero << showbase << endl;
+}
+
+
